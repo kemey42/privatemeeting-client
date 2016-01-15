4a69fdcc10d96859e33db381345aa7521a3f4381
kemey42 <akmalzamel@gmail.com>
2016-01-15 23:04:32 +0800

    merge scheduler into index page

--- js/index.js
+++ js/index.js
@@ -10,19 +10,30 @@ $(function() {
   });
 });
 
-// function ParticipantEmail(String email) {
-//   var table = document.getElementById("tableEmail");
-//   var tableBody = table.getElementsByTagName("tbody").item(0);
-//   var tbRows = tableBody.rows.length;
-//
-//   if (email==undefined  && tbRows>0){
-//     tableBody.deleteRow(-1);
-//     return;
-//   }
-//
-//   var row = tableBody.insertRow(-1);
-//   var cell1 = row.insertCell(0);
-//   var cell2 = row.insertCell(1);
-//   var cell3 = row.insertCell(2);
-//
-// }
+function AddParticipantEmail() {
+  var email = document.getElementById("addEmail").value;
+  var table = document.getElementById("tableEmail");
+  var tableBody = table.getElementsByTagName("tbody").item(0);
+  var tbRows = tableBody.rows.length;
+
+  if (email=="" || email.indexOf('@')===-1 || email.indexOf(' ')>=0){
+    return;
+  }
+
+  var row = tableBody.insertRow(-1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell1.innerHTML = tbRows+1;
+  cell2.innerHTML = email.toLowerCase();
+  cell3.innerHTML = '<span class="delX" onclick="deleteRow(this)">&times</span>';
+
+  document.getElementById("addEmail").value = "";
+}
+
+function deleteRow(r) {
+  var table = document.getElementById("tableEmail");
+  var tableBody = table.getElementsByTagName("tbody").item(0);
+  var i = r.parentNode.parentNode.rowIndex;
+  tableBody.deleteRow(i-1);
+}
