ea4b3dbf76f615ee458d03862b6d0eee2f7ff4c2
kemey42 <akmalzamel@gmail.com>
2016-01-26 17:43:12 +0800

    ignore node_modules

--- js/index.js
+++ js/index.js
@@ -10,13 +10,14 @@ $(function() {
   });
 });
 
-function AddParticipantEmail() {
+function addParticipantEmail() {
   var email = document.getElementById("addEmail").value;
   var table = document.getElementById("tableEmail");
   var tableBody = table.getElementsByTagName("tbody").item(0);
   var tbRows = tableBody.rows.length;
 
-  if (email=="" || email.indexOf('@')===-1 || email.indexOf(' ')>=0){
+  if (!validateEmail(email)) {
+    console.log(email + " email entered invalid");
     return;
   }
 
@@ -24,7 +25,7 @@ function AddParticipantEmail() {
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
-  cell1.innerHTML = tbRows+1;
+  //cell1.innerHTML = tbRows+1;
   cell2.innerHTML = email.toLowerCase();
   cell3.innerHTML = '<span class="delX" onclick="deleteRow(this)">&times</span>';
 
@@ -35,5 +36,50 @@ function deleteRow(r) {
   var table = document.getElementById("tableEmail");
   var tableBody = table.getElementsByTagName("tbody").item(0);
   var i = r.parentNode.parentNode.rowIndex;
-  tableBody.deleteRow(i-1);
+  tableBody.deleteRow(i - 1);
+}
+
+function clearSchedule() {
+  document.getElementById("inputMeetName").value = "";
+  document.getElementById("inputMeetDesc").value = "";
+  document.getElementById("inputMeetDate").value = "";
+  document.getElementById("inputMeetTime").value = "";
+  $("#tableEmail tbody tr").remove();
+}
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function sendInvite() {
+  var table = document.getElementById("tableEmail");
+  var tableBody = table.getElementsByTagName("tbody").item(0);
+  var strEmail = "";
+
+  for (var r=0, n=tableBody.rows.length; r < n; r++){
+    var p = r+1;
+    strEmail = strEmail.concat("Participant " + p.toString() + " <" + tableBody.rows[r].cells[1].innerHTML + ">");
+    if(p<n){
+      strEmail = strEmail.concat(", ")
+    }
+  }
+  console.log(strEmail);
+
+  var email = require("email.js");
+  var server = email.server.connect({
+    user: "akmalzamel.fyputp.noreply",
+    password: "password",
+    host: "smtp.gmail.com",
+    ssl: true
+  });
+  
+  server.send({
+    text: "Online Meeting Created",
+    from: "MC App <akmalzamel.fyputp.noreply@gmail.com>",
+    to: strEmail,
+    subject: "Email created"
+  }, function(err, message) {
+    console.log(err || message);
+  });
 }
